4b1064f7ec55301e4d8511e6b355c906e188a6e5
FloatingFront <floatingfront@gmail.com>
2019-06-21 12:01:26 +0100

    Basic functionality for reading json file

--- elxr/apps/elxrtree/lib/Elxrtree/cli.ex
+++ elxr/apps/elxrtree/lib/Elxrtree/cli.ex
@@ -16,9 +16,46 @@ defmodule Elxrtree.CLI do
     :world
   end
 
+#  def f(arg), do: IO.inspect arg
+
+#  System.argv()
+#  |> Enum.inspect(&Elxrtree.CLI.f/1)
+
   def main(args) do
-    {opts,_,_}= OptionParser.parse(args,switches: [file: :string], aliases: [f: :file])
+
+    {opts,_,_} = OptionParser.parse_head(args,switches: [file: :string, group: :string], aliases: [f: :file, g: :group])
+
+    IO.puts "opts:"
     IO.inspect opts
+
+    [opt| opts] = opts
+
+    IO.puts "opt:"
+    IO.inspect opt
+    IO.puts "opts:"
+    IO.inspect opts
+
+    {file, filename} =  opt
+
+    IO.puts "file(1):"
+    IO.inspect file
+    IO.puts "filename:"
+    IO.inspect filename
+
+    {:ok, file} = File.open(filename)
+
+    IO.puts "file(2):"
+    IO.inspect file
+
+    IO.puts "filecontent(1):"
+    {:ok, filecontent} = File.read(filename)
+
+    IO.puts "filecontent(2):"
+    IO.puts filecontent
+
+#    case flag do
+#      {}
+#    end
   end 
    
    
