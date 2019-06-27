83dad44d039fdf24d7997b7f3763e5a7860c20b6
FloatingFront <floatingfront@gmail.com>
2019-06-27 11:46:46 +0100

    Work in progress - logging functions...

--- elxr/apps/elxrtree/lib/Elxrtree/cli.ex
+++ elxr/apps/elxrtree/lib/Elxrtree/cli.ex
@@ -16,21 +16,35 @@ defmodule Elxrtree.CLI do
     :world
   end
 
-  def do_fareport(fajsonstructure)  do
+  def handle_filecontent(json_structure)  do
      level = 0
      preamble = "*"
      count = 0
 
-     IO.puts "fajsonstructure:"
-#     IO.inspect fajsonstructure
+     IO.puts "json_structure:"
+#     IO.inspect json_structure
 
-     decodedfajsonstructure = Poison.decode(fajsonstructure)
+     decoded_json_structure = Poison.decode(json_structure)
 #      |> IO.inspect
-      |> handle_struct
+      |> handle_decoded_json_struct
   end
 
-  def handle_struct({:ok, content }) do
-     IO.puts "handle_struct"
+  def handle_decoded_json_struct({:ok, content }) do
+     log_where(__ENV__)
+     IO.puts "Test1"
+     %{function: {name,arity}}  = __ENV__ 
+     IO.puts "Test2"
+     IO.puts "where : #{name}"
+     IO.puts "Test3"
+#     IO.puts "where3: #{String.replace_suffix(String.replace_prefix(where,"{:",""),"}","")}" 
+#     IO.puts "where4: #{String.split(where,["{:",",","}"])}"
+#     String.replace_prefix(where,"{","")
+#       |> String.replace_suffix() 
+#     IO.puts String.splitter(where,["{",",","}"]) 
+
+#     log_where( where )
+#     IO.puts ": #{Map.keys(inspect(__ENV__.function))} :"
+#     IO.puts ": #{Map.to_list(inspect(__ENV__.function))} :"
      [treestruct | summary ] = content
      [summarystruct | rest]  = summary
 #     IO.inspect summarystruct
@@ -39,11 +53,16 @@ defmodule Elxrtree.CLI do
      handle_treestruct(treestruct) 
   end
 
-  def handle_struct({_,_}) do
-    IO.puts "Something went wrong:handle_struct 2"
+  def handle_decoded_json_struct({_,_}) do
+    IO.puts "Something went wrong:handle_decoded_json_struct 2"
   end
 
+  def log_where( %{function: {function_name,function_arity}} ) do
+    IO.puts "log_where #{function_name} #{function_arity}"
+  end   
+
   def handle_treestruct(treestruct) do
+     log_where(__ENV__)
      IO.puts "handle_treestruct"
 #     IO.puts "treestruct-1"
 #     IO.inspect treestruct
@@ -55,20 +74,21 @@ defmodule Elxrtree.CLI do
      IO.inspect treestruct["contents"]
 #     IO.puts "treestruct-2"
 #     IO.inspect treestruct
+# TODO : move start of break-up up to here...
      handle_content(treestruct["contents"],0,0)
   end
 
   def handle_content(nil,[],count) do
-     
+      IO.puts ":handle_content  : nil"     
   end
   
   def handle_content(content,level,count) do
-      IO.puts "handle_content"     
+     log_where(__ENV__)
+      IO.puts ":handle_content  :"     
       count = count + 1
       IO.puts "count: #{count}"
       IO.puts "level: #{inspect(level)}"
-
-#      IO.inspect content
+      IO.puts ":content          : #{inspect(content)}"
       [content_first | content_rest] = content
       content_type = get_in(content_first, ["type"] )
       IO.puts ":content_type     :  #{inspect(content_type)}"
@@ -84,16 +104,25 @@ defmodule Elxrtree.CLI do
       end
   end
 
-  def handle_directory(content_rest,[],level,count) do
+  def handle_directory([], content_rest, level, count) do
     IO.puts ":handle_directory : nil" 
     handle_content(content_rest, (level - 1), count)
   end
 
   def handle_directory(content_first, content_rest, level, count) do
+    level = level + 1
     IO.puts ":handle_directory : #{inspect(content_first)}" 
-    IO.puts get_in(content_first, ["name"])
-    handle_content(content_rest, (level + 1), count)
+    IO.puts ":name             : #{get_in(content_first, ["name"])}"
+     
+    handle_content(content_first, (level), count)
+    handle_content(content_rest, (level), count)
   end
+
+  def handle_file([], content_rest, level, count) do
+    IO.puts ":handle_file      : nil" 
+    handle_content(content_rest, (level), count)
+  end
+
 
   def handle_file(content_first, content_rest, level, count) do
     IO.puts ":handle_file      : #{inspect(content_first)}" 
@@ -113,6 +142,9 @@ defmodule Elxrtree.CLI do
   def fareport_directory() do
   end
 
+  @doc """
+  Entry point for the elxrtree utility.
+  """
   def main(args) do
 
     {opts,_,_} = OptionParser.parse_head(
@@ -145,7 +177,7 @@ defmodule Elxrtree.CLI do
     IO.puts "filecontent: #{filecontent}"
 
 
-    do_fareport(filecontent)
+    handle_filecontent(filecontent)
   end 
 
 end
