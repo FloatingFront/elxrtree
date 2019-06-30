32a60b1a166c4fbe69f38e69f91c18314a7d271c
FloatingFront <floatingfront@gmail.com>
2019-06-30 09:04:36 +0100

    Cleaning an restructuring - making more elixir-ish.

--- elxr/apps/elxrtree/lib/Elxrtree/cli.ex
+++ elxr/apps/elxrtree/lib/Elxrtree/cli.ex
@@ -18,69 +18,58 @@ defmodule Elxrtree.CLI do
 
   def handle_filecontent(json_structure)  do
      log_where(__ENV__)
-     decoded_json_structure = Poison.decode(json_structure)
+#     decoded_json_structure = 
+     Poison.decode(json_structure)
       |> IO.inspect 
       |> handle_decoded_json_struct
   end
 
   def handle_decoded_json_struct({:ok, content }) do
      log_where(__ENV__)
+
 #     %{function: {name,arity}}  = __ENV__ 
 #     IO.puts "where : #{name}"
 
-     [struct|struct_rest] = content
-
-     [dir_struct | summary ] = content
-
-     [summarystruct | rest]  = summary
-
-     IO.puts "content        : #{inspect content}\n"
-     IO.puts "struct         : #{inspect struct}\n"
-     IO.puts "struct_rest    : #{inspect struct_rest}\n"
-     IO.puts "dir_struct     : #{inspect dir_struct}\n"
-     IO.puts "summary        : #{inspect summary}\n"
-     IO.puts "summarystructs : #{inspect summarystruct}\n"
-     IO.puts "rest           : #{inspect rest}\n"
-#     handle_dir_struct(dir_struct) 
+     IO.puts "content          : #{inspect content}\n"
      handle_struct(content)
   end
 
   def handle_struct([struct_head|struct_rest]) do
      log_where(__ENV__,"list-head")
-     IO.puts "struct_head    : #{inspect struct_head}\n"
-     IO.puts "struct_rest    : #{inspect struct_rest}\n"
+     IO.puts "struct_head      : #{inspect struct_head}\n"
+     IO.puts "struct_rest      : #{inspect struct_rest}\n"
      handle_struct(struct_head)
      handle_struct(struct_rest)
   end
 
   def handle_struct([] = content) do
     log_where(__ENV__,"list-empty")
-    IO.puts "content         : #{inspect content}\n" 
+    IO.puts "content           : #{inspect content}\n" 
   end
 
   def handle_struct(%{"type" => "file"} = content) do
     log_where(__ENV__,"map-file")
-    IO.puts "content         : #{inspect content}\n" 
+    IO.puts "content-file      : #{inspect content}\n" 
   end
 
   def handle_struct(%{"type" => "directory", "contents" => dir_contents ,"name" => name} = content) do
     log_where(__ENV__,"map-directory")
-    IO.puts "name            : #{name}"
-    IO.puts "type            : directory"
-    IO.puts "content         : #{inspect dir_contents}\n" 
+    IO.puts "name              : #{name}"
+    IO.puts "type              : directory"
+    IO.puts "content           : #{inspect dir_contents}\n" 
     handle_struct(dir_contents) 
   end
 
   def handle_struct(%{"type" => "report", "directories" => directories ,"files" => files} = content) do
     log_where(__ENV__,"map-report")
-    IO.puts "directories     : #{directories}"
-    IO.puts "type            : report"
-    IO.puts "files           : #{files}\n"
+    IO.puts "directories       : #{directories}"
+    IO.puts "type              : report"
+    IO.puts "files             : #{files}\n"
   end
 
   def handle_struct(%{} = content) do
     log_where(__ENV__,"map-empty")
-    IO.puts "content         : #{inspect content}\n" 
+    IO.puts "content           : #{inspect content}\n" 
   end
 
   def handle_struct( %{type: "file", name: name, content: content }) do
@@ -104,54 +93,11 @@ defmodule Elxrtree.CLI do
     IO.puts "==== #{function_name} ==== #{function_arity} ==== #{type}"
   end   
 
-  def handle_dir_struct(dir_struct) do
-     log_where(__ENV__)
-     IO.puts "name    : #{inspect dir_struct["name"]}"
-     IO.puts "type    : #{inspect dir_struct["type"]}"
-     IO.puts "contents: #{inspect dir_struct["contents"]}"
-# TODO : move start of break-up up to here...
-     handle_content(dir_struct["contents"],0,0)
-  end
-
   def handle_content(nil,[],count) do
      log_where(__ENV__)
      IO.puts ":handle_content  : nil"     
   end
   
-  def handle_content(content,level,count) do
-      log_where(__ENV__)
-      count = count + 1
-      IO.puts "count: #{count}"
-      IO.puts "level: #{inspect(level)}"
-      IO.puts ":content          : #{inspect(content)}"
-      [content_first | content_rest] = content
-      content_type = get_in(content_first, ["type"] )
-      IO.puts ":content_type     :  #{inspect(content_type)}"
-      IO.puts ":content_first    :  #{inspect(content_first)}"
-      IO.puts ":content_rest     :  #{inspect(content_rest)}"
-      case content_type do
-         "directory"    -> handle_directory(content_first, content_rest, level, count) #  IO.inspect get_in(content_first, ["name"])
-         "file"         -> handle_file(content_first, content_rest, level, count) #  IO.inspect get_in(content_first, ["name"])
-         _              -> IO.puts "something tripped up"
-      end
-  end
-
-  def handle_directory([], content_rest, level, count) do
-     log_where(__ENV__)
-     IO.puts ":handle_directory : nil" 
-     handle_content(content_rest, (level - 1), count)
-  end
-
-  def handle_directory(content_first, content_rest, level, count) do
-     log_where(__ENV__)
-     level = level + 1
-     IO.puts ":handle_directory : #{inspect(content_first)}" 
-     IO.puts ": name            : #{get_in(content_first, ["name"])}"
-     IO.puts ": type            : #{get_in(content_first, ["type"])}"
-     handle_content(content_first, (level), count)
-     handle_content(content_rest, (level), count)
-  end
-
   def handle_file([], content_rest, level, count) do
      log_where(__ENV__)
      IO.puts ":handle_file      : nil" 
@@ -163,13 +109,6 @@ defmodule Elxrtree.CLI do
      IO.puts ":handle_file      : #{inspect(content_first)}" 
      IO.puts get_in(content_first, ["name"])
      handle_content(content_rest, (level), count)
-  end
-
-  def fareport(preamble, jsonbit, jsonbits, level, count) do
-     log_where(__ENV__)
-     IO.puts jsonbit
-     [jsonbit|jsonbits]= jsonbits
-     fareport(preamble,jsonbit, jsonbits, level, count)
   end
 
   def report_file() do
