c881ae5d814334e206a778831e32c3a113ac5bf0
FloatingFront <floatingfront@gmail.com>
2019-07-10 22:00:23 +0100

    Tidying up a little, argument parsing.

--- elxr/apps/elxrtree/lib/Elxrtree/cli.ex
+++ elxr/apps/elxrtree/lib/Elxrtree/cli.ex
@@ -33,7 +33,6 @@ defmodule Elxrtree.CLI do
 
   def handle_filecontent(json_structure) do
 #    log_where(__ENV__)
-    #     decoded_json_structure = 
     Poison.decode(json_structure)
 #    |> IO.inspect()
     |> handle_decoded_json_struct
@@ -42,41 +41,28 @@ defmodule Elxrtree.CLI do
   def handle_decoded_json_struct({:ok, content}) do
 #    log_where(__ENV__)
     dir_level=0
-    #     %{function: {name,arity}}  = __ENV__ 
-    #     IO.puts "where : #{name}"
     IO.puts "#{CssStyle.apply_css_style()}"
     IO.puts "#{CssStyle.apply_div_begin()}"
-#    IO.puts("content          : #{inspect(content)}\n")
     handle_struct(content, dir_level)
     IO.puts "#{CssStyle.apply_div_end()}"
   end
 
   def handle_struct([struct_head | struct_rest], dir_level)  do
 #    log_where(__ENV__, "list-head")
-#    IO.puts("dir-level         : #{dir_level}")
-#    IO.puts("struct_head      : #{inspect(struct_head)}\n")
-#    IO.puts("struct_rest      : #{inspect(struct_rest)}\n")
     handle_struct(struct_head, dir_level)
-
     handle_struct(struct_rest, dir_level)
   end
 
   def handle_struct([] = content, dir_level) do
 #    log_where(__ENV__, "list-empty")
     dir_level = dir_level - 1
-#    IO.puts("dir-level         : #{dir_level}")
-#    IO.puts("content           : #{inspect(content)}\n")
-    IO.puts "#{CssStyle.apply_list_items_directory_end}"
     IO.puts "#{CssStyle.apply_unordered_list_end}"
+    IO.puts "#{CssStyle.apply_list_items_directory_end}"
   end
 
   def handle_struct(%{"type" => "file", "name" => name} = content, dir_level) do
 #    log_where(__ENV__, "map-file")
 #    log_file(content, dir_level)
-#    IO.puts("dir-level         : #{dir_level}")
-#    IO.puts("name              : #{name}")
-#    IO.puts("type              : file")
-#    IO.puts("content-file      : #{inspect(content)}\n")
     IO.puts "#{CssStyle.apply_list_item_file(name)}"
     
   end
@@ -87,13 +73,8 @@ defmodule Elxrtree.CLI do
 #    log_where(__ENV__, "map-directory")
     dir_level = dir_level + 1
 #    log_directory(content, dir_level)
-#    IO.puts "#{name}"
     IO.puts "#{CssStyle.apply_list_items_directory_begin(name)}"
     IO.puts "#{CssStyle.apply_unordered_list_begin}"
-#    IO.puts("dir-level         : #{dir_level}")
-#    IO.puts("name              : #{name}")
-#    IO.puts("type              : directory")
-#    IO.puts("content           : #{inspect(dir_contents)}\n")
     handle_struct(dir_contents, dir_level)
   end
 
@@ -103,25 +84,13 @@ defmodule Elxrtree.CLI do
 #    log_where(__ENV__, "map-report")
 #    IO.puts("dir-level         : #{dir_level}")
     IO.puts("Directories       : #{directories}")
-#    IO.puts("type              : report")
-    IO.puts(" Files             : #{files}\n")
+    IO.puts("Files             : #{files}\n")
   end
 
   def handle_struct(%{} = content, dir_level) do
     log_where(__ENV__, "map-empty")
-#    IO.puts("dir-level         : #{dir_level}")
-#    IO.puts("content           : #{inspect(content)}\n")
   end
 
-  def handle_struct(%{type: "file", name: name, content: content}, dir_level) do
-    log_where(__ENV__, "file")
-    IO.puts("dir-level         : #{dir_level}")
-  end
-
-  def handle_struct([struct_head | struct_rest], dir_level) do
-    log_where(__ENV__, "rest")
-    IO.puts("dir-level         : #{dir_level}")
-  end
 
   def handle_decoded_json_struct({_, _}) do
     log_where(__ENV__)
@@ -155,12 +124,6 @@ defmodule Elxrtree.CLI do
     IO.puts(":handle_content  : nil")
   end
 
-  def handle_file([], content_rest, level, count) do
-    log_where(__ENV__)
-    IO.puts(":handle_file      : nil")
-    handle_content(content_rest, level, count)
-  end
-
   def handle_file(content_first, content_rest, level, count) do
 #    log_where(__ENV__)
 #    IO.puts(":handle_file      : #{inspect(content_first)}")
@@ -168,28 +131,24 @@ defmodule Elxrtree.CLI do
     handle_content(content_rest, level, count)
   end
 
-  def report_file() do
-    log_where(__ENV__)
-  end
-
-  def report_directory() do
-    log_where(__ENV__)
-  end
-
   @doc """
   Entry point for the elxrtree utility.
   """
   def main(args) do
-    {opts, _, _} =
-      OptionParser.parse_head(
+#    {opts, _, _} =
+    {parsed, remaining, errors} =
+#      OptionParser.parse_head(
+      OptionParser.parse(
         args,
-        switches: [file: :string, group: :string],
-        aliases: [f: :file, g: :group]
+        switches: [file: :string, help: :boolean],
+        aliases: [f: :file, h: :help]
       )
 
-#    IO.puts("opts: #{inspect(opts)}")
+#    IO.puts("#{inspect(opts)}")
+    IO.puts("parsed: #{inspect(parsed)} - remaining: #{inspect(remaining)} - errors:#{inspect(errors)}" )
 
-    [opt | opts] = opts
+#    [opt | opts] = opts
+    [opt | opts] = parsed 
 
 #    IO.puts("opt: #{inspect(opt)} ")
 #    IO.puts("opts: #{inspect(opts)}")
